5797e7a01525292ee56e83f848cc597554d5f56e
Paul Rainsford <paulrainsford69@gmail.co>
2021-09-28 15:48:02 +0100

    Created Account class, default balance and test

---
+++ lib/account.rb
@@ -0,0 +1,15 @@
+class Account
+
+  attr_reader :balance
+  
+  def initialize
+    @balance = 0
+  end
+
+
+
+
+
+
+
+end
---
+++ spec/units/account_spec.rb
@@ -0,0 +1,20 @@
+require 'account'
+
+describe Account do
+
+  context '#balance' do
+    it "should have default balance of zero" do
+      expect(subject.balance).to eq(0)
+    end
+  end
+
+
+
+
+
+
+
+
+
+
+end
